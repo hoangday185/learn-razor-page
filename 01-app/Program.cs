512a2d0e4aba48e7eefb4998b09a6c076deb594e
hoangday185 <jarvietnguyen318@gmail.com>
2025-01-10 15:54:13 +0700

    create project, syntax, layout of razor page

---
+++ 01-app/Program.cs
@@ -0,0 +1,14 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints => { endpoints.MapGet("/", async (context) =>
+{
+    await context.Response.WriteAsync("Trang chủ");
+})})
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
